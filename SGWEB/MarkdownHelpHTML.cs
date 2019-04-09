37af415a499d8e98183f290752a78fbea3b6e6ed
Anthony <anthony@custombit.co.uk>
2019-04-09 16:02:29 +0100

    Added markup reference HTML when editing in the Posts pages

---
+++ SGWEB/MarkdownHelpHTML.cs
@@ -0,0 +1,33 @@
+namespace SGWEB
+{
+    public class MarkdownHelpHTML
+    {
+		public const string RawHTML = @"
+
+<div id=""postHelpText"" class=""collapse SGHelpText"">
+<h1>Markup Syntax</h1>
+<p>
+Here's an example of the simple markup syteax supported:
+</p>
+<pre>
+# Biggest Heading 
+## Heading Level 2
+##3 Heading Level 3
+
+Paragraph text with *bold*, ^italic^, and _underline_.
+New lines are honoured
+
+Blank lines create new paragraphs
+
+- Bullet item 1
+- Bullet item 2
+- Bullet item 3
+
+#. Enumerated item 1
+#. Enumerated item 2
+#. Enumerated item 3
+</pre>
+</div>
+";
+    }
+}
